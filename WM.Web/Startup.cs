6dccbc68d96d7810e779f4ed051b4aa0465f458d
Zoran Jezdimirovic <zoran.jezdimirovic@gmail.com>
2020-05-10 13:04:31 +0200

    Init migration and some UI

--- WM.Web/Startup.cs
+++ WM.Web/Startup.cs
@@ -37,8 +37,10 @@ namespace WM.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplContext applContext)
         {
+            applContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
